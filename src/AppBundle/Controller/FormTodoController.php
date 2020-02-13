5ad53a8cb1fe534911f88b4462c2af73080038d7
Umar Faruk <umarfaruk@Faruks-MacBook-Pro.local>
2020-02-13 19:22:50 +0530

    removed comments

--- src/AppBundle/Controller/FormTodoController.php
+++ src/AppBundle/Controller/FormTodoController.php
@@ -6,20 +6,31 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Todo;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class FormTodoController extends Controller
 {
+
+    /**
+     * @Route("/", name="homepage")
+     * @param Request $request
+     * @return Response
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('todos/index.html.twig');
+    }
+    /**
+     * @Route("/newtodo", name="New_Todo")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\JsonResponse|Response
+     */
     public function newAction(Request $request)
     {
-    // creates a task and gives it some dummy data for this example
-//        $todo = new Todo();
-//        $todo->setName('todo another');
-//        $todo->setDescription('descrkrtorejogje');
-
-
         $todo = new Todo();
 
         $form = $this->createFormBuilder($todo)
@@ -42,13 +53,39 @@ class FormTodoController extends Controller
              $entityManager = $this->getDoctrine()->getManager();
              $entityManager->persist($todo);
              $entityManager->flush();
-            $todoArray=['name'=> $todo->getName(), 'desc'=> $todo->getDescription(),'priority'=> $todo->getPriority()];
+             $todoArray=['name'=> $todo->getName(), 'desc'=> $todo->getDescription(),'priority'=> $todo->getPriority()];
             # new Response("");
             return $this->json($todoArray);
             #return $this->redirectToRoute('task_success');
         }
 
 
-        return $this->render('newtodo.html.twig', array('form' => $form->createView(),));
+        return $this->render('todos/newtodo.html.twig', array('form' => $form->createView()));
     }
+
+    /**
+     * @Route("/todo/delete/{id}", name="Todo_Delete")
+     * @param $id
+     * @return Response
+     */
+    public function deleteAction($id){
+
+
+        $entityManager = $this->getDoctrine()->getManager();
+        #$deleteTodo = $entityManager->getRepository(Todo::class)->findOneByName('This 1');
+        $deleteTodo = $entityManager->getRepository(Todo::class)->find($id);
+        #deleting by id
+
+        $entityManager->remove($deleteTodo);
+        $entityManager->flush();
+
+        
+        return new Response('Deleted todo with name '.$deleteTodo->getName().
+            '<a href="/todo/display" class="btn btn-danger">Show all todos!</a>');
+
+    }
+
+
+
+
 }
